7f8a165fd76d65daec272a89323ec72bc15a021d
shichao <jsc0218@gmail.com>
2019-11-17 12:51:54 -0500

    delete unnecessary scripts, let's start from this version

--- util/build_version.cc
+++ util/build_version.cc
@@ -1,4 +1,4 @@
 #include "build_version.h"
-const char* rocksdb_build_git_sha = "rocksdb_build_git_sha:912b172f1ab39802ff5f92d37f57687eed00b021";
+const char* rocksdb_build_git_sha = "rocksdb_build_git_sha:360c1798118ec9970afbf3f39c0f8639ab659c01";
 const char* rocksdb_build_git_date = "rocksdb_build_git_date:2019-11-13";
 const char* rocksdb_build_compile_date = __DATE__;
