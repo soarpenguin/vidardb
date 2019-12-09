0c2a764e69dbfe622169eeb11394a933aa10c3de
shichao <jsc0218@gmail.com>
2019-12-09 16:47:19 -0500

    remove db_compaction_filter_test

--- util/build_version.cc
+++ util/build_version.cc
@@ -1,4 +1,4 @@
 #include "build_version.h"
-const char* rocksdb_build_git_sha = "rocksdb_build_git_sha:66d3e65c8d914dea35f105a1c9a1881989f8593b";
-const char* rocksdb_build_git_date = "rocksdb_build_git_date:2019-12-03";
+const char* rocksdb_build_git_sha = "rocksdb_build_git_sha:d44bb81b6c5899bdfbab3f69c8a106e821f1aa20";
+const char* rocksdb_build_git_date = "rocksdb_build_git_date:2019-12-09";
 const char* rocksdb_build_compile_date = __DATE__;
