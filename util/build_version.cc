2a83c908597e83f5d53e172b02e1c9590dae96d1
shichao <jsc0218@gmail.com>
2020-01-12 10:27:08 -0500

    slightly format example code

--- util/build_version.cc
+++ util/build_version.cc
@@ -1,4 +1,4 @@
 #include "build_version.h"
-const char* rocksdb_build_git_sha = "rocksdb_build_git_sha:6764ce1a91bf63244ab47fe96635fe2a76d1b668";
+const char* rocksdb_build_git_sha = "rocksdb_build_git_sha:c50fc5cf211f5ab0546495143d8c50bdc852f6b8";
 const char* rocksdb_build_git_date = "rocksdb_build_git_date:2020-01-12";
 const char* rocksdb_build_compile_date = __DATE__;
