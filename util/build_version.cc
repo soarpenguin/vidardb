9c8011b223d81e2a0a55501cefef9ca3a69872b8
chenquanzhao <quanzhao.cqz@gmail.com>
2020-01-12 14:47:59 +0800

    archive test codes

--- util/build_version.cc
+++ util/build_version.cc
@@ -1,4 +1,4 @@
 #include "build_version.h"
-const char* rocksdb_build_git_sha = "rocksdb_build_git_sha:6764ce1a91bf63244ab47fe96635fe2a76d1b668";
+const char* rocksdb_build_git_sha = "rocksdb_build_git_sha:d0e8356be2b4f252faeb0c8ebba37ff5fc0ab97d";
 const char* rocksdb_build_git_date = "rocksdb_build_git_date:2020-01-12";
 const char* rocksdb_build_compile_date = __DATE__;
