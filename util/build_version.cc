d6fed2ba11f926165aaedf24fcafbc2dfa9e68f4
chenquanzhao <quanzhao.cqz@gmail.com>
2020-01-18 00:10:03 +0800

    remove arcanist_util

--- util/build_version.cc
+++ util/build_version.cc
@@ -1,4 +1,4 @@
 #include "build_version.h"
-const char* vidardb_build_git_sha = "vidardb_build_git_sha:73ae052c87015d8adfe9bbdaaa37fa67d09bc488";
-const char* vidardb_build_git_date = "vidardb_build_git_date:2020-01-16";
+const char* vidardb_build_git_sha = "vidardb_build_git_sha:b649a425628518b6944ee900c0bd2f0ed005f657";
+const char* vidardb_build_git_date = "vidardb_build_git_date:2020-01-18";
 const char* vidardb_build_compile_date = __DATE__;
