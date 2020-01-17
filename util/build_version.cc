89eefacd3ff4a86e2dca4011b255545e3fec8b8b
shichao <jsc0218@gmail.com>
2020-01-17 16:32:09 -0500

    fix bugs in rangequery

--- util/build_version.cc
+++ util/build_version.cc
@@ -1,4 +1,4 @@
 #include "build_version.h"
-const char* vidardb_build_git_sha = "vidardb_build_git_sha:73ae052c87015d8adfe9bbdaaa37fa67d09bc488";
-const char* vidardb_build_git_date = "vidardb_build_git_date:2020-01-16";
+const char* vidardb_build_git_sha = "vidardb_build_git_sha:22444b5b088944a815c0d19a22eaa4f76b679e6e";
+const char* vidardb_build_git_date = "vidardb_build_git_date:2020-01-17";
 const char* vidardb_build_compile_date = __DATE__;
